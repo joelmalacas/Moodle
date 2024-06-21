ef2a8a7f08c18b525cc7c908e169d58d1779c506
joelmalacas <joel.malacas@ipcbcampus.pt>
2024-06-21 18:07:03 +0100

    Update Structure Style

--- Scripts/bd.php
+++ Scripts/bd.php
@@ -4,7 +4,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $database = "moodle";
+    $database = "moodle_accounts";
 
     $conn = mysqli_connect($servername, $username, $password, $database);
 
