a6315cc68d13f9b1d181b714f00f8b940d17023a
joelmalacas <joel.malacas@ipcbcampus.pt>
2024-07-07 00:37:16 +0100

    Update

--- Recover/code.php
+++ Recover/code.php
@@ -4,12 +4,17 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $codigo = $_POST["code"];
 
+
+    if ($_SESSION['numero_quatro_digitos'] == null || $_SESSION['Recover_email'] == null) {
+        print '<script>window.location.href="http://localhost/Moodle/"</script>';
+    }
+
     if (isset($_POST['Confirma'])) {
         if ($codigo == $_SESSION['numero_quatro_digitos']) {
             echo '<script>window.location.href="NewPassword.html"</script>';
         } else {
-            echo '<script>alert("Código errado!")</script>';
-            print '<script>window.location.reload();</script>';
+            print '<script>alert("Código inserido é Inválido")</script>';
+            print '<script>window.location.href="http://localhost/Moodle/Recover/code.html"</script>';
         }
     }
 }
